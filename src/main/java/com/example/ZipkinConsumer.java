be3ad715a46aa9accb74bb7cf0af4e7287c638ec
ethanlin013 <ethanlinlyc@gmail.com>
2016-12-02 11:58:40 -0800

    customsonsumer

---
+++ src/main/java/com/example/ZipkinConsumer.java
@@ -0,0 +1,13 @@
+//package com.example;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+//
+//@SpringBootApplication
+//@EnableZipkinStreamServer
+//public class ZipkinConsumer {
+//	public static void main(String[] args) {
+//		SpringApplication.run(ZipkinConsumer.class, args);
+//	}
+//}
